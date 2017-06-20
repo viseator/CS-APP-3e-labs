c93a920226a9e1dcb1c6556cddb2d885a71b6af2
viseator <viseator@gmail.com>
2017-06-20 20:38:58 +0800

    ready to defuse secret bomb

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -1304,36 +1304,64 @@ Disassembly of section .text:
   40114b:	7e e8                	jle    401135 <phase_6+0x41>
   40114d:	49 83 c5 04          	add    $0x4,%r13
   401151:	eb c1                	jmp    401114 <phase_6+0x20>
+
+rsi = rsp + 0x18
   401153:	48 8d 74 24 18       	lea    0x18(%rsp),%rsi
+r14 = rsp
+rax = rsp
   401158:	4c 89 f0             	mov    %r14,%rax
+ecx = 0x7
+edx = 0x7
   40115b:	b9 07 00 00 00       	mov    $0x7,%ecx
   401160:	89 ca                	mov    %ecx,%edx
+edx = 0x7 - a1
   401162:	2b 10                	sub    (%rax),%edx
+a1 = 0x7 - a1
   401164:	89 10                	mov    %edx,(%rax)
+rax = rsp + 4
   401166:	48 83 c0 04          	add    $0x4,%rax
+
   40116a:	48 39 f0             	cmp    %rsi,%rax
   40116d:	75 f1                	jne    401160 <phase_6+0x6c>
+args = 7 - args
+
+esi = 0
   40116f:	be 00 00 00 00       	mov    $0x0,%esi
   401174:	eb 21                	jmp    401197 <phase_6+0xa3>
+
   401176:	48 8b 52 08          	mov    0x8(%rdx),%rdx
   40117a:	83 c0 01             	add    $0x1,%eax
   40117d:	39 c8                	cmp    %ecx,%eax
   40117f:	75 f5                	jne    401176 <phase_6+0x82>
   401181:	eb 05                	jmp    401188 <phase_6+0x94>
+
   401183:	ba d0 32 60 00       	mov    $0x6032d0,%edx
+  *(rsp+2*rsi+0x20) = 0x6032d0
   401188:	48 89 54 74 20       	mov    %rdx,0x20(%rsp,%rsi,2)
+rsi = rsi + 4
   40118d:	48 83 c6 04          	add    $0x4,%rsi
   401191:	48 83 fe 18          	cmp    $0x18,%rsi
+rsi = 0x18
   401195:	74 14                	je     4011ab <phase_6+0xb7>
+
+ecx = *(rsp + rsi)
   401197:	8b 0c 34             	mov    (%rsp,%rsi,1),%ecx
+ecx <= 1
   40119a:	83 f9 01             	cmp    $0x1,%ecx
   40119d:	7e e4                	jle    401183 <phase_6+0x8f>
+
   40119f:	b8 01 00 00 00       	mov    $0x1,%eax
   4011a4:	ba d0 32 60 00       	mov    $0x6032d0,%edx
   4011a9:	eb cb                	jmp    401176 <phase_6+0x82>
+
+
+rbx = 1'
   4011ab:	48 8b 5c 24 20       	mov    0x20(%rsp),%rbx
+rax = rsp + 0x28 (&2')
   4011b0:	48 8d 44 24 28       	lea    0x28(%rsp),%rax
+rsi = rsp + 0x50 (&6')
   4011b5:	48 8d 74 24 50       	lea    0x50(%rsp),%rsi
+
   4011ba:	48 89 d9             	mov    %rbx,%rcx
   4011bd:	48 8b 10             	mov    (%rax),%rdx
   4011c0:	48 89 51 08          	mov    %rdx,0x8(%rcx)
@@ -1342,14 +1370,18 @@ Disassembly of section .text:
   4011cb:	74 05                	je     4011d2 <phase_6+0xde>
   4011cd:	48 89 d1             	mov    %rdx,%rcx
   4011d0:	eb eb                	jmp    4011bd <phase_6+0xc9>
+
   4011d2:	48 c7 42 08 00 00 00 	movq   $0x0,0x8(%rdx)
   4011d9:	00 
+
   4011da:	bd 05 00 00 00       	mov    $0x5,%ebp
+
   4011df:	48 8b 43 08          	mov    0x8(%rbx),%rax
   4011e3:	8b 00                	mov    (%rax),%eax
   4011e5:	39 03                	cmp    %eax,(%rbx)
   4011e7:	7d 05                	jge    4011ee <phase_6+0xfa>
   4011e9:	e8 4c 02 00 00       	callq  40143a <explode_bomb>
+
   4011ee:	48 8b 5b 08          	mov    0x8(%rbx),%rbx
   4011f2:	83 ed 01             	sub    $0x1,%ebp
   4011f5:	75 e8                	jne    4011df <phase_6+0xeb>
